0dbc01e2b9fcf4020af4a9647c13f3a4d5c58272
Florin B <florinbizgan2000@gmail.com>
2022-05-19 22:15:48 +0000

    Clean

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-function incredientsHover() {
-  document.get.ElementById('incredients').firstElementChild.firstElementChild.style.FontSize = "300%";
-}
-function incredientsNormal() {
-  document.getElementById('incredients').firstElementChild.firstElementChild.style.FontSize = "100%";
-}
-function preparationHover() {
-  document.get.ElementById('preparation').firstElementChild.firstElementChild.style.FontSize = "300%";
-}
-function preparationNormal() {
-  document.getElementById('preparation').firstElementChild.firstElementChild.style.FontSize = "100%";
-}
-
-function showTime() {
-  document.getElementById('currentTime').innerHTML = new Date().toUTCString();
-}
 showTime();
 setInterval(function() {
   showTime();
